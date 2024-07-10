a7347c10439837a346b50819eb5cf3b25f12aac9
Sabina Liljeström <sabinaliljestrom@Sabinas-MacBook-Pro-2.local>
2024-07-10 12:43:58 +0200

    lagt in icon för skidoreter

--- src/main.js
+++ src/main.js
@@ -2,6 +2,17 @@ import L from 'leaflet';
 import './css/main.scss';
 import skiResorts from './ski-resorts.json'; 
 
+// Importera ikonen
+import skiIconUrl from './assets/images/icon.png';
+
+// Skapa en Leaflet-ikon
+const skiIcon = L.icon({
+    iconUrl: skiIconUrl,
+    iconSize: [32, 32], // Justera storleken på ikonen efter behov
+    iconAnchor: [16, 32], // Justera ankaret så att ikonen placeras korrekt
+    popupAnchor: [0, -32] // Justera popup-ankaret så att popupen visas ovanför ikonen
+});
+
 // Initialisera kartan
 const map = L.map('map').setView([63.0, 15.0], 5); // Koordinater för Sverige
 
@@ -13,7 +24,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // Funktion för att visa skidorter
 function showSkiResorts() {
     skiResorts.forEach(resort => {
-        const marker = L.marker([resort.lat, resort.lon]).addTo(map)
+        const marker = L.marker([resort.lat, resort.lon],{ icon: skiIcon }).addTo(map)
         .bindPopup(`<b>${resort.name}</b>`);
     });
 }
