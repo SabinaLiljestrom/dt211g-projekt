90a62c738fd00f0b40530efa21cee05feb17d82f
Sabina Liljeström <sabinaliljestrom@Sabinas-MacBook-Pro-2.local>
2024-07-10 13:50:13 +0200

    uppdaterat json färre decimaler koordinater för att matcha smhi api samt lagttill funktion för att skriva ut temperatur i rutan som öppnas

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import L from 'leaflet';
 import './css/main.scss';
 import skiResorts from './ski-resorts.json'; 
-
-// Importera ikonen
 import skiIconUrl from './assets/images/icon.png';
 
 // Skapa en Leaflet-ikon
@@ -20,13 +18,38 @@ const map = L.map('map').setView([63.0, 15.0], 5); // Koordinater för Sverige
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
-
 // Funktion för att visa skidorter
 function showSkiResorts() {
     skiResorts.forEach(resort => {
-        const marker = L.marker([resort.lat, resort.lon],{ icon: skiIcon }).addTo(map)
-        .bindPopup(`<b>${resort.name}</b>`);
+        const marker = L.marker([resort.lat, resort.lon], { icon: skiIcon }).addTo(map)
+            .bindPopup(`<b>${resort.name}</b><br>Hämtar väder...`, { closeOnClick: false, autoClose: false });
+
+        marker.on('click', () => {
+            showWeather(resort.name, resort.lat, resort.lon, marker);
+        });
     });
+}
+// Funktion för att hämta och visa väderprognos och lavininfo
+async function showWeather(resortName, lat, lon, marker) {
+    try {
+        const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+        const category = 'pmp3g';
+        const version = '2';
+        const apiUrl = `https://opendata-download-metfcst.smhi.se/api/category/${category}/version/${version}/geotype/point/lon/${lon}/lat/${lat}/data.json`;
+
+        const weatherResponse = await fetch(proxyUrl + apiUrl);
+        const weatherText = await weatherResponse.text(); // Hämta råtext
+        console.log(weatherText); // Logga den råa texten för att se vad vi får
+        const weatherData = JSON.parse(weatherText); // Försök att parsa texten till JSON
+
+        const temperature = weatherData.timeSeries[0].parameters.find(param => param.name === 't').values[0];
+
+        const popupContent = `<b>${resortName}</b><br>Temperatur: ${temperature}°C`;
+        marker.getPopup().setContent(popupContent).update();
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        marker.getPopup().setContent(`<b>${resortName}</b><br>Kunde inte hämta väderdata.`).update();
+    }
 }
 
 // Visa skidorter vid start
